fc9891e6d8b2e39e1c5c48e1351fccae2da2e4a0
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-11-16 09:33:41 +0100

    remove double ;

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import del from 'rollup-plugin-delete';
 
 import terser from '@rollup/plugin-terser';
 
-import pkg from './package.json' assert { type: "json" };;
+import pkg from './package.json' assert { type: "json" };
 
 export default [
   {
